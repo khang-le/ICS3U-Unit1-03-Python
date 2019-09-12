74eab3465c4588f1225d670e73b06704bf41c843
Ubuntu <ubuntu@ip-172-31-30-187.ec2.internal>
2019-09-12 18:46:02 +0000

    new code

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Created by: Khang Le
+#Created on: September 12, 2019
+#This is the "Hello, World!" program, but in a standard form
+
+
+def main():
+    print ("Hello, World!")
+    
+    
+if __name__ == "__main__":
+    main()
